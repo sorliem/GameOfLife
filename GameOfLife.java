1040700b37f120a55271cb62504e1055a4d2071a
Miles Sorlie <sorliem@kenteches.com>
2014-09-16 10:29:41 -0600

    Now using two global boolean arrays to store previous and current generation

--- GameOfLife.java
+++ GameOfLife.java
@@ -1,28 +1,26 @@
 package gameoflife;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import javax.swing.JFrame;
-import javax.swing.Timer;
 
 /**
  *
  * @author Miles Sorlie
  */
-public class GameOfLife extends JFrame implements ActionListener {
+public class GameOfLife extends JFrame  {
 
     GamePanel panel;
-    static boolean board[][];
+    boolean curBoard[][];
+    boolean prevBoard[][];
     int panelWidth, panelHeight;
-    private static final int DELAY = 450;
-    Timer timer = new Timer(DELAY, this);
+    
 
     public GameOfLife() throws FileNotFoundException {
         super("Game of Life");
-        readBoard();
+        String fileName = "start_board_3.in";
+        readBoard(fileName);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         panel = new GamePanel(this, panelWidth, panelHeight);
         getContentPane().add(panel);
@@ -32,23 +30,25 @@ public class GameOfLife extends JFrame implements ActionListener {
         setVisible(true);
         printBoard();
         
-        timer.start();
+        
         while (true) {
             runGame();
         }
     }
 
-    private void readBoard() throws FileNotFoundException {
-        Scanner in = new Scanner(new File("start_board_3.in"));
+    private void readBoard(String fileName) throws FileNotFoundException {
+        Scanner in = new Scanner(new File(fileName));
         panelWidth = in.nextInt();
         panelHeight = in.nextInt();
         in.nextLine();
-        board = new boolean[panelWidth][panelHeight];
+        curBoard = new boolean[panelWidth][panelHeight];
+        prevBoard = new boolean[panelWidth][panelHeight];
         String line = in.nextLine();
         for (int i = 0; i < panelWidth; i++) {
             for (int j = 0; j < panelHeight; j++) {
                 if (line.charAt(j) == 'x') {
-                    board[i][j] = true;
+                    curBoard[i][j] = true;
+                    prevBoard[i][j] = true;
                 }
             }
             if (in.hasNextLine()) {
@@ -58,9 +58,9 @@ public class GameOfLife extends JFrame implements ActionListener {
     }
     
     private void printBoard() {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (board[i][j]) 
+        for (int i = 0; i < curBoard.length; i++) {
+            for (int j = 0; j < curBoard[0].length; j++) {
+                if (curBoard[i][j]) 
                     System.out.print("x");
                 else    
                     System.out.print(" ");
@@ -79,7 +79,7 @@ public class GameOfLife extends JFrame implements ActionListener {
      * live neighbours becomes a live cell, as if by reproduction.
      */
     public void runGame() {
-        boolean[][] prevBoard = board;
+        copyBoards();
         for (int i = 0; i < prevBoard.length; i++) {
             for (int j = 0; j < prevBoard[0].length; j++) {
                 int numLiveNeighbors = 0;
@@ -88,6 +88,7 @@ public class GameOfLife extends JFrame implements ActionListener {
                         if (k != 0 || l != 0) {
                             try {
                                 if (prevBoard[i + k][j + l]) {
+//                                    System.out.println("i + k = " + (i + k) + ", j + l = " + (j + l));
                                     numLiveNeighbors++;
                                 }
                             } catch (IndexOutOfBoundsException e) {
@@ -98,28 +99,31 @@ public class GameOfLife extends JFrame implements ActionListener {
                 }
 
                 if (!prevBoard[i][j] && numLiveNeighbors == 3) {
-                    board[i][j] = true;
+                    curBoard[i][j] = true;
                 } else if (prevBoard[i][j] && numLiveNeighbors == 1) {
-                    board[i][j] = false;
+                    curBoard[i][j] = false;
                 } else if (prevBoard[i][j] && numLiveNeighbors == 2) {
-                    board[i][j] = true;
+                    curBoard[i][j] = true;
                 } else if (prevBoard[i][j] && numLiveNeighbors == 3) {
-                    board[i][j] = true;
+                    curBoard[i][j] = true;
                 } else if (prevBoard[i][j] && numLiveNeighbors > 3) {
-                    board[i][j] = false;
+                    curBoard[i][j] = false;
                 } else {
-                    board[i][j] = false;
+                    curBoard[i][j] = false;
                 }
             }
         }
 
     }
     
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == timer) {
-            panel.repaint();
+    private void copyBoards() {
+        for (int i = 0; i < prevBoard.length; i++) {
+            for (int j = 0; j < prevBoard[0].length; j++) {
+                prevBoard[i][j] = curBoard[i][j];
+            }
         }
     }
+    
+   
 
 }
--- GamePanel.java
+++ GamePanel.java
@@ -1,11 +1,10 @@
 package gameoflife;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.io.FileNotFoundException;
 import javax.swing.JPanel;
 import javax.swing.Timer;
@@ -14,11 +13,15 @@ import javax.swing.Timer;
  *
  * @author Miles Sorlie
  */
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements ActionListener {
 
     public GameOfLife game;
+    private static final int DELAY = 500;
+    Timer timer = new Timer(DELAY, this);
 
     public GamePanel(GameOfLife gol, int width, int height) throws FileNotFoundException {
+        timer.start();
+        setBackground(new Color(255, 255, 255));
         setPreferredSize(new Dimension(width * 7, height * 7));
         game = gol;
     }
@@ -27,15 +30,22 @@ public class GamePanel extends JPanel {
     public void paint(Graphics g) {
         int xLoc = 0;
         int yLoc = 0;
-        for (int i = 0; i < game.board.length; i++) {
+        for (int i = 0; i < game.curBoard.length; i++) {
             xLoc = 0; // reset x coord to left side of panel
-            for (int j = 0; j < game.board[0].length; j++) {
-                if (game.board[i][j]) {
+            for (int j = 0; j < game.curBoard[0].length; j++) {
+                if (game.curBoard[i][j]) {
                     g.fillRect(xLoc, yLoc, 7, 7);
                 }
                 xLoc += 7;
             }
             yLoc += 7;
+        }
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent ev){
+        if(ev.getSource()==timer){
+          repaint();// this will call at every 1 second
         }
     }
 }
